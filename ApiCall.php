6d9dbc2e12a3130d5f2c509994a324580c6b17e1
temi724 <>
2017-02-17 14:04:00 +0100

    this is a very simple application that generate the list of books stored in our shelf

---
+++ ApiCall.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/humanity/jquery-ui.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<?php include ('ApiCall.php')?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>list of books</title>
+    </head>
+    <body>
+        <div class="container 1">
+            <div class="jumbotron">
+                <h3>some contents here later</h3>
+            </div>
+            </div>
+        <div class="container 2">
+            <!---creating a grid system-->
+            <div class="row">
+                <div class="col col-md-4">
+                                            <!-- Large modal -->
+<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+      
+  </div>
+</div>
+<!--modal 2-->
+                        <!-- Large modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+      
+  </div>
+</div><!--modal 2 ends-->
+                    
+                </div><!--coll ends-->
+                
+            </div><!--row ends-->
+
+        
+            </div><!--container-->
+        
+    </body>
+</html>
